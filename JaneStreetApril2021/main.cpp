8a102112012fea426b35ed619e923f8c244c0267
Eli Rose <erose@getnerdio.com>
2023-09-22 11:34:37 -0400

    Update to main.cpp to avoid using objects.

--- JaneStreetApril2021/main.cpp
+++ JaneStreetApril2021/main.cpp
@@ -1,4 +1,4 @@
-#include "common.h"
+#include <stdio.h>
 
 int findIndex(int arr[], int size, int value) {
     for (int i = 0; i < size; i++) {
@@ -11,7 +11,7 @@ int findIndex(int arr[], int size, int value) {
 
 int intPow(int m, int n) {
     if (!n) return 1;
-    return m * intPow(m, n-1);
+    return m * intPow(m, n - 1);
 }
 
 void swapElements(int arr[], int index1, int index2) {
@@ -20,12 +20,12 @@ void swapElements(int arr[], int index1, int index2) {
     arr[index2] = temp;
 }
 
-double getProbability(int arr[], int round, int index, map<pair<int, int>, double>& memo) {
+double getProbability(int arr[], int round, int index, double memo[16][16]) {
     if (!round) {
-        if (index % 2) return double(arr[index - 1]) / double(arr[index] + arr[index - 1]);
-        else return double(arr[index + 1]) / double(arr[index] + arr[index + 1]);
+        if (index % 2) return (double)arr[index - 1] / (double)(arr[index] + arr[index - 1]);
+        else return (double)arr[index + 1] / (double)(arr[index] + arr[index + 1]);
     }
-    if (memo.count({ round,index })) return memo[{round, index}];
+    if (memo[round][index] != 0.0) return memo[round][index];
     int x1 = intPow(2, round);
     int x2 = 2 * x1;
     int i;
@@ -33,40 +33,47 @@ double getProbability(int arr[], int round, int index, map<pair<int, int>, doubl
     if (index % x2 >= x1) {
         i = x1 * (index / x1);
         for (int j = 0; j < x1; j++) {
-            res += getProbability(arr, round - 1, i - j - 1, memo) * double(arr[i - j - 1])/double(arr[i - j - 1] + arr[index]);
+            res += getProbability(arr, round - 1, i - j - 1, memo) * (double)arr[i - j - 1] / (double)(arr[i - j - 1] + arr[index]);
         }
     }
     else {
         i = x1 * (index / x1 + 1);
         for (int j = 0; j < x1; j++) {
-            res += getProbability(arr, round - 1, i + j, memo) * double(arr[i + j]) / double(arr[i + j] + arr[index]);
+            res += getProbability(arr, round - 1, i + j, memo) * (double)arr[i + j] / (double)(arr[i + j] + arr[index]);
         }
     }
     res = res * getProbability(arr, round - 1, index, memo);
-    memo[{round, index}] = res;
+    memo[round][index] = res;
     return res;
 }
 
 int main() {
-	int bracket[16] = {1, 16, 8, 9, 5, 12, 4, 13, 6, 11, 3, 14, 7, 10, 2, 15};
-    map<pair<int, int>, double> memo;
-    double prob, bestProb = 0, initProb = getProbability(bracket, 3, 14, memo);
-    cout << "The initial probability is " << initProb << endl;
-    pair<int, int> bestSwap;
+    int bracket[16] = { 1, 16, 8, 9, 5, 12, 4, 13, 6, 11, 3, 14, 7, 10, 2, 15 };
+    double memo[16][16] = { 0 }; // Initialize memoization table to zeros
+    double prob, bestProb = 0;
+    double initProb = getProbability(bracket, 3, 14, memo);
+    printf("The initial probability is %lf\n", initProb);
+    int bestSwap[2] = { 0 };
     for (int i = 0; i < 16; i++) {
-        for (int j = i+1; j < 16; j++) {
-            memo.clear();
+        for (int j = i + 1; j < 16; j++) {
+            // Clear memoization table for each iteration
+            for (int k = 0; k < 16; k++) {
+                for (int l = 0; l < 16; l++) {
+                    memo[k][l] = 0;
+                }
+            }
             swapElements(bracket, i, j);
             int index = findIndex(bracket, 16, 2);
             prob = getProbability(bracket, 3, index, memo);
-            cout << "Swapping " << bracket[i] << " and " << bracket[j] << " yields a probability of " << prob << endl;
+            printf("Swapping %d and %d yields a probability of %lf\n", bracket[i], bracket[j], prob);
             if (prob > bestProb) {
                 bestProb = prob;
-                bestSwap = {i ,j};
+                bestSwap[0] = i;
+                bestSwap[1] = j;
             }
             swapElements(bracket, i, j);
         }
     }
-    cout << "The best swap is " << bracket[bestSwap.first] << " and " << bracket[bestSwap.second] << " with a probability increase of " << bestProb - initProb << endl;
+    printf("The best swap is %d and %d, with a probability increase of %lf\n", bracket[bestSwap[0]], bracket[bestSwap[1]], bestProb - initProb);
     return 0;
 }
